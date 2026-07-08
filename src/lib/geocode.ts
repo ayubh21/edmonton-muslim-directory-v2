bb9516368f20c90973dcc0ddbb93d88fda6b4c20
ayubh21 <you@example.com>
2026-07-07 20:02:44 -0600

    attempting to restrict geocode to server requests only

--- src/lib/geocode.ts
+++ src/lib/geocode.ts
@@ -1,45 +1,48 @@
-import { fromLatLng } from "react-geocode";
-
 interface LatLng {
-	lat: number;
-	lng: number;
+  lat: number;
+  lng: number;
 }
 
 const GOOGLE_GEOCODE_API = "https://maps.googleapis.com/maps/api/geocode";
 
 export class Geocode {
-	apiKey: string;
+  apiKey: string;
 
-	constructor(apiKey: string) {
-		this.apiKey = apiKey;
-	}
-	// TODO restrict range in map form
-	async getCoordinates(address: string) {
-		const result = await fetch(
-			`${GOOGLE_GEOCODE_API}/json?address=${address}&key=${this.apiKey}`
-		);
-		const data = await result.json();
-		console.log(data)
-		if (!data) {
-			return
-		}
-		return data.results[0].geometry.location as LatLng;
-	}
-	async getAddress(coordinates: LatLng) {
-		const result = await fetch(
-			`${GOOGLE_GEOCODE_API}/json?latlng=${coordinates.lat}, ${coordinates.lng}&key=${this.apiKey}`
-		);
-		const data = await result.json();
-		return data
-	}
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+  // TODO restrict range in map form
+  async getCoordinates(address: string) {
+    const result = await fetch(
+      `${GOOGLE_GEOCODE_API}/json?address=${address}&key=${this.apiKey}`,
+    );
+    const data = await result.json();
+    console.log(data);
+    if (!data) {
+      return;
+    }
+    return data.results[0].geometry.location as LatLng;
+  }
 
-	async getFromPlaceId(placeId: string) {
-		const result = await fetch(`https://places.googleapis.com/v1/places/${placeId}?fields=id,displayName&key=${this.apiKey}`)
-		const data = await result.json();
-		return data;
-	}
+  async getAddress(coordinates: LatLng) {
+    try {
+      const result = await fetch(
+        `${GOOGLE_GEOCODE_API}/json?latlng=${coordinates.lat}, ${coordinates.lng}&key=${this.apiKey}`,
+      );
+      const data = await result.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFromPlaceId(placeId: string) {
+    const result = await fetch(
+      `https://places.googleapis.com/v1/places/${placeId}?fields=id,displayName&key=${this.apiKey}`,
+    );
+    const data = await result.json();
+    return data;
+  }
 }
 
-export const geocode = new Geocode(
-	process.env.GCLOUD_GEOCODE_API_KEY!
-);
+export const geocode = new Geocode(process.env.GCLOUD_GEOCODE_API_KEY!);
